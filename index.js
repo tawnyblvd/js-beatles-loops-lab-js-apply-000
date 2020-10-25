07fe72941ddea7ff1e6fe14861a70b6eb6c726cb
Ariel <cantegone@yahoo.com>
2020-10-25 01:27:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // add solution here
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
+function theBeatlesPlay(arr1, arr2) {
+  let beatlesLove = [];
+  for (let counter = 0; counter < arr1.length; counter++) {
+    beatlesLove.push(`${arr1[counter]} plays ${arr2[counter]}`);
+  }
+  return beatlesLove;
+}
+console.log(theBeatlesPlay(musicians, instruments));
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+function johnLennonFacts(arr) {
+  let i = 0;
+  let exclamationArray = [];
+  while(arr[i] < arr[arr.length]) {
+    exclamationArray.push(arr[i] + "!!!");
+  }
+  return exclamationArray;
+}
+console.log(johnLennonFacts(facts));
+
+function iLoveTheBeatles(num) {
+  let love = [];
+  do {
+    love.push("I love the Beatles!");
+  }
+  while (num < 15);
+  num++;
+  return love;
+}
+
+console.log(iLoveTheBeatles(10));
